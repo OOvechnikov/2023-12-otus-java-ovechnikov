881429f7c8194653449bab591132a2b3d7b1586f
Ovechnikov <warlordkolomna@bk.ru>
2024-04-08 23:19:18 +0300

    Homework 10 init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ include("hw07-patterns")
 include("hw08-io")
 include("hw09-jdbc:demo")
 include("hw09-jdbc:homework")
+include("hw10-jpql")
 
 pluginManagement {
     val jgitver: String by settings
