d0cacbab9cb71e471ed44cd4a1f96b12368b5975
oovechnikov <ovechnikovok@gmail.com>
2024-05-06 21:52:21 +0300

    hw13

--- hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,23 +1,75 @@
 package ru.otus.appcontainer;
 
-import java.util.*;
-
+import lombok.SneakyThrows;
+import lombok.val;
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.function.Consumer;
 
 @SuppressWarnings("squid:S1068")
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
-    private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
     public AppComponentsContainerImpl(Class<?> initialConfigClass) {
         processConfig(initialConfigClass);
     }
 
+    @SneakyThrows
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        initContext(configClass);
+    }
+
+    private void initContext(Class<?> configClass) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        val instance = configClass.getConstructor().newInstance();
+        Arrays.stream(configClass.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(AppComponent.class))
+                .sorted(componentOrderComparator())
+                .forEachOrdered(createBeanAndPutInContext(instance));
+    }
+
+    private Consumer<Method> createBeanAndPutInContext(Object instance) {
+        return method -> {
+            val bean = createBean(instance, method);
+            val beanName = getBeanName(method);
+            val previousBean = appComponentsByName.put(beanName, bean);
+            if (previousBean != null) {
+                throw new IllegalArgumentException("Several beans with name: %s".formatted(beanName));
+            }
+        };
+    }
+
+    private Comparator<Method> componentOrderComparator() {
+        return (m1, m2) -> {
+            int order1 = m1.getAnnotation(AppComponent.class).order();
+            int order2 = m2.getAnnotation(AppComponent.class).order();
+            return Integer.compare(order1, order2);
+        };
+    }
+
+    @SneakyThrows
+    private Object createBean(Object instance, Method m) {
+        val beansToInject = Arrays.stream(m.getParameterTypes())
+                .map(this::getAppComponent)
+                .toList();
+        if (beansToInject.isEmpty()) {
+            return m.invoke(instance);
+        }
+        return m.invoke(instance, beansToInject.toArray());
+    }
+
+    private String getBeanName(Method method) {
+        var beanName = method.getAnnotation(AppComponent.class).name();
+        if (!beanName.isEmpty()) {
+            return beanName;
+        }
+        return method.getReturnType().getSimpleName();
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -28,12 +80,21 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        val beans = appComponentsByName.values().stream()
+                .filter(v -> v.getClass().equals(componentClass) || Arrays.asList(v.getClass().getInterfaces()).contains(componentClass))
+                .toList();
+        if (beans.isEmpty()) {
+            throw new IllegalArgumentException("There is no beans in context with class: %s.".formatted(componentClass));
+        } else if (beans.size() > 1) {
+            throw new IllegalArgumentException("There are more than one beans in context with class: %s.".formatted(componentClass));
+        }
+        return (C) beans.get(0);
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) Optional.ofNullable(appComponentsByName.get(componentName))
+                .orElseThrow();
     }
 
 }
