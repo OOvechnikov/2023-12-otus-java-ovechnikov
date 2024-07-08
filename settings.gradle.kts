bf2cb0e88e3dcb7499f42122583d2bf0fbe28f6d
oovechnikov <ovechnikovok@gmail.com>
2024-07-08 20:26:44 +0300

    hw17

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ include("hw13-di")
 include("hw14-springDataJdbc")
 include("hw15-executors")
 include("hw16-concurrentCollections")
+include("hw17-gRPCServer")
 
 pluginManagement {
     val jgitver: String by settings
